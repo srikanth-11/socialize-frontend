57fdfdf87168062a9851112abbdc876b8b62920a
srikanth <kasireddysrikanth27@gmail.com>
2021-03-15 20:20:18 +0530

    switch added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import SimpleCard from './components/home/home'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SinglePost from './components/singlepost/singlepost'
 import CreatePost from './components/createpost/createpost'
 import AlertTemplate from "react-alert-template-basic";
@@ -13,23 +13,24 @@ import ProtectedRoute from './components/protected-route/protectedroute'
 import Landingpage from './components/landingpage/landingpage'
 
 function App() {
- const options = {
-  timeout: 3000,
-  position: positions.TOP_RIGHT
-};
- return<> <Provider template={AlertTemplate} {...options}> <Router>
- 
-  <ProtectedRoute exact path="/posts/:postId" component={SinglePost} />
-  <ProtectedRoute exact path="/app" component={SimpleCard} />
-  <ProtectedRoute exact path="/createpost" component={CreatePost}/>
-  <Route exact path="/signup" component={Signup} />
-   <Route exact path="/login" component={Login} />
-   <Route exact path="/forgotpassword" component={Forgotpassword} />
-        <Route path="/reset-password/:resetToken" component={Resetpassword} />
-        <Route exact path="/" component={Landingpage}/>
- </Router>
- </Provider>
- </>
+  const options = {
+    timeout: 3000,
+    position: positions.TOP_RIGHT
+  };
+  return <> <Provider template={AlertTemplate} {...options}> <Router>
+    <Switch>
+      <ProtectedRoute exact path="/posts/:postId" component={SinglePost} />
+      <ProtectedRoute exact path="/app" component={SimpleCard} />
+      <ProtectedRoute exact path="/createpost" component={CreatePost} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/forgotpassword" component={Forgotpassword} />
+      <Route path="/reset-password/:resetToken" component={Resetpassword} />
+      <Route exact path="/" component={Landingpage} />
+    </Switch>
+  </Router>
+  </Provider>
+  </>
 }
 
 export default App;
